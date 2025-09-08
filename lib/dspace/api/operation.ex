f053e98e5b180fb85ef2c412420e971a6204b267
Moritz F. Fuerst <moritz.fuerst@gmail.com>
2025-09-08 22:25:07 +0200

    Adds Operation protocol

---
+++ lib/dspace/api/operation.ex
@@ -0,0 +1,15 @@
+defprotocol DSpace.Api.Operation do
+  @moduledoc """
+  Defines a protocol for executing operations on a DSpace API.
+  """
+
+  @doc """
+  Performs a synchronous request on a DSpace API, returning a result or an error.
+  """
+  def perform(operation, client, options)
+
+  @doc """
+  Performs a streaming request on a DSpace API.
+  """
+  def stream!(operation, client, options)
+end
