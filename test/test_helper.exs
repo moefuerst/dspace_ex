d2b9c9bcc4b0562bc07a60ef5edf87842dec423b
Moritz F. Fuerst <moritz.fuerst@gmail.com>
2025-09-08 22:27:50 +0200

    Test infrastructure

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,51 @@
+defmodule TestHelper do
+  @moduledoc false
+
+  defmodule Http do
+    @moduledoc false
+    @behaviour DSpace.Api.Http
+
+    @impl true
+    def request(options) do
+      send(self(), {:http_request, options})
+
+      status = Keyword.get(options, :test_return_status, 200)
+      headers = Keyword.get(options, :test_return_headers, %{"content-type" => ["application/json"]})
+      body = Keyword.get(options, :test_return_body, %{"response" => "ok"})
+
+      {:ok, %DSpace.Api.Http.Response{status: status, headers: headers, body: body}}
+    end
+  end
+
+  @doc """
+  Helper to respond with JSON content type and a pre-formatted JSON string body (for use with Bypass)
+  """
+  def respond_with_json(conn, status_code, json_body_string) do
+    conn
+    |> Plug.Conn.put_resp_content_type("application/json")
+    |> Plug.Conn.resp(status_code, json_body_string)
+  end
+
+  @doc """
+  Loads a JSON fixture as a string (for use with Bypass)
+  """
+  def load_fixture(path) do
+    ["test", "support", "fixtures", path]
+    |> Path.join()
+    |> File.read!()
+  end
+
+  @doc """
+  Parses a JSON fixture (direct assertions)
+  """
+  def parse_fixture(path) do
+    path
+    |> load_fixture()
+    |> Jason.decode!()
+  end
+end
+
+ExUnit.configure(exclude: :external)
 ExUnit.start()
 
 Application.ensure_all_started(:bypass)
