044b8abed19706009fb4c6c206cc02d75c5d5170
Moritz F. Fuerst <moritz.fuerst@gmail.com>
2025-10-31 17:15:30 +0100

    Updates mix project
    Introduced mise configuration. Updated dependencies. Added precommit alias to follow conventions

--- .credo.exs
+++ .credo.exs
@@ -1,9 +1,4 @@
-# This file contains the configuration for Credo and you are probably reading
-# this after creating it with `mix credo.gen.config`.
-#
-# If you find anything wrong or unclear in this file, please report an
-# issue on GitHub: https://github.com/rrrene/credo/issues
-#
+# This file contains the configuration for Credo
 %{
   #
   # You can have as many configs as you like in the `configs:` field.
@@ -31,7 +26,7 @@
           "apps/*/test/",
           "apps/*/web/"
         ],
-        excluded: [~r"/_build/", ~r"/deps/", ~r"/node_modules/"]
+        excluded: [~r"/_build/", ~r"/deps/", ~r"/assets/", ~r"/node_modules/"]
       },
       #
       # Load and configure plugins here:
@@ -64,6 +59,13 @@
       #
       #     {Credo.Check.Design.DuplicatedCode, false}
       #
+      # You can customize the priority of any check
+      # Priority values are: `low, normal, high, higher`
+      #
+      # You can also customize the exit_status of each check.
+      # If you don't want a check to cause `mix credo` to fail, just
+      # set this value to 0 (zero).
+      #
       checks: %{
         enabled: [
           #
@@ -71,70 +73,124 @@
           #
           {Credo.Check.Consistency.ExceptionNames, []},
           {Credo.Check.Consistency.LineEndings, []},
-          {Credo.Check.Consistency.ParameterPatternMatching, []},
           {Credo.Check.Consistency.SpaceAroundOperators, []},
           {Credo.Check.Consistency.SpaceInParentheses, []},
           {Credo.Check.Consistency.TabsOrSpaces, []},
+          {Credo.Check.Consistency.MultiAliasImportRequireUse, []},
+          {Credo.Check.Consistency.ParameterPatternMatching, []},
 
           #
           ## Design Checks
           #
-          # You can customize the priority of any check
-          # Priority values are: `low, normal, high, higher`
-          #
-          {Credo.Check.Design.AliasUsage,
-           [priority: :low, if_nested_deeper_than: 2, if_called_more_often_than: 0]},
+          {Credo.Check.Design.AliasUsage, [if_nested_deeper_than: 2, if_called_more_often_than: 1]},
           {Credo.Check.Design.TagFIXME, []},
-          # You can also customize the exit_status of each check.
-          # If you don't want TODO comments to cause `mix credo` to fail, just
-          # set this value to 0 (zero).
-          #
           {Credo.Check.Design.TagTODO, [exit_status: 0]},
+          {Credo.Check.Design.SkipTestWithoutComment, []},
 
           #
           ## Readability Checks
           #
-          {Credo.Check.Readability.AliasOrder, []},
+          {Credo.Check.Readability.BlockPipe, [exit_status: 0]},
           {Credo.Check.Readability.FunctionNames, []},
-          {Credo.Check.Readability.LargeNumbers, []},
-          {Credo.Check.Readability.MaxLineLength, [priority: :low, max_length: 120]},
+          {Credo.Check.Readability.MaxLineLength, [priority: :low, max_length: 110, exit_status: 0]},
           {Credo.Check.Readability.ModuleAttributeNames, []},
-          {Credo.Check.Readability.ModuleDoc, []},
           {Credo.Check.Readability.ModuleNames, []},
           {Credo.Check.Readability.ParenthesesInCondition, []},
-          {Credo.Check.Readability.ParenthesesOnZeroArityDefs, []},
-          {Credo.Check.Readability.PipeIntoAnonymousFunctions, []},
           {Credo.Check.Readability.PredicateFunctionNames, []},
-          {Credo.Check.Readability.PreferImplicitTry, []},
           {Credo.Check.Readability.RedundantBlankLines, []},
           {Credo.Check.Readability.Semicolons, []},
           {Credo.Check.Readability.SpaceAfterCommas, []},
-          {Credo.Check.Readability.StringSigils, []},
+          {Credo.Check.Readability.StrictModuleLayout,
+           order: [
+             :shortdoc,
+             :moduledoc,
+             :behaviour,
+             :use,
+             :import,
+             :alias,
+             :require,
+             :module_attribute,
+             :defstruct,
+             :type,
+             :typep,
+             :opaque,
+             :callback,
+             :macrocallback,
+             :optional_callbacks
+           ]},
           {Credo.Check.Readability.TrailingBlankLine, []},
           {Credo.Check.Readability.TrailingWhiteSpace, []},
-          {Credo.Check.Readability.UnnecessaryAliasExpansion, []},
           {Credo.Check.Readability.VariableNames, []},
+          {Credo.Check.Readability.AliasOrder, []},
+          {Credo.Check.Readability.LargeNumbers, []},
+          {Credo.Check.Readability.ModuleDoc, []},
+          {Credo.Check.Readability.MultiAlias, []},
+          {Credo.Check.Readability.OneArityFunctionInPipe, []},
+          {Credo.Check.Readability.ParenthesesOnZeroArityDefs, []},
+          {Credo.Check.Readability.PipeIntoAnonymousFunctions, []},
+          {Credo.Check.Readability.PreferImplicitTry, []},
+          {Credo.Check.Readability.SinglePipe, []},
+          {Credo.Check.Readability.StringSigils, []},
+          {Credo.Check.Readability.UnnecessaryAliasExpansion, []},
           {Credo.Check.Readability.WithSingleClause, []},
+          {ExSlop.Check.Readability.NarratorDoc, []},
+          {ExSlop.Check.Readability.BoilerplateDocParams, []},
+          {ExSlop.Check.Readability.ObviousComment, [additional_keywords: []]},
+          {ExSlop.Check.Readability.StepComment, []},
+          {ExSlop.Check.Readability.NarratorComment, []},
+          {ExSlop.Check.Readability.UnaliasedModuleUse, []},
 
           #
           ## Refactoring Opportunities
           #
           {Credo.Check.Refactor.Apply, []},
-          {Credo.Check.Refactor.CondStatements, []},
+          {Credo.Check.Refactor.AppendSingleItem, []},
           {Credo.Check.Refactor.CyclomaticComplexity, []},
-          {Credo.Check.Refactor.FilterCount, []},
+          {Credo.Check.Refactor.DoubleBooleanNegation, []},
           {Credo.Check.Refactor.FilterFilter, []},
           {Credo.Check.Refactor.FunctionArity, []},
           {Credo.Check.Refactor.LongQuoteBlocks, []},
-          {Credo.Check.Refactor.MapJoin, []},
           {Credo.Check.Refactor.MatchInCondition, []},
+          {Credo.Check.Refactor.MapMap, []},
+          {Credo.Check.Refactor.Nesting, []},
+          {Credo.Check.Refactor.RejectReject, []},
+          {Credo.Check.Refactor.UtcNowTruncate, []},
+          {Credo.Check.Refactor.CaseTrivialMatches, []},
+          {Credo.Check.Refactor.CondStatements, []},
+          {Credo.Check.Refactor.FilterCount, []},
+          {Credo.Check.Refactor.MapJoin, []},
           {Credo.Check.Refactor.NegatedConditionsInUnless, []},
           {Credo.Check.Refactor.NegatedConditionsWithElse, []},
-          {Credo.Check.Refactor.Nesting, []},
+          {Credo.Check.Refactor.PipeChainStart, []},
           {Credo.Check.Refactor.RedundantWithClauseResult, []},
-          {Credo.Check.Refactor.RejectReject, []},
           {Credo.Check.Refactor.UnlessWithElse, []},
           {Credo.Check.Refactor.WithClauses, []},
+          {ExSlop.Check.Refactor.FilterNil, []},
+          {ExSlop.Check.Refactor.RejectNil, []},
+          {ExSlop.Check.Refactor.ReduceAsMap, []},
+          {ExSlop.Check.Refactor.MapIntoLiteral, []},
+          {ExSlop.Check.Refactor.IdentityPassthrough, []},
+          {ExSlop.Check.Refactor.IdentityMap, []},
+          {ExSlop.Check.Refactor.CaseTrueFalse, []},
+          {ExSlop.Check.Refactor.TryRescueWithSafeAlternative, []},
+          {ExSlop.Check.Refactor.WithIdentityElse, []},
+          {ExSlop.Check.Refactor.WithIdentityDo, []},
+          {ExSlop.Check.Refactor.SortThenReverse, []},
+          {ExSlop.Check.Refactor.StringConcatInReduce, []},
+          {ExSlop.Check.Refactor.ReduceMapPut, []},
+          {ExSlop.Check.Refactor.RedundantBooleanIf, []},
+          {ExSlop.Check.Refactor.FlatMapFilter, []},
+          {ExSlop.Check.Refactor.RedundantEnumJoinSeparator, []},
+          {ExSlop.Check.Refactor.UseMapJoin, []},
+          {ExSlop.Check.Refactor.PreferEnumSlice, []},
+          {ExSlop.Check.Refactor.GraphemesLength, []},
+          {ExSlop.Check.Refactor.ManualStringReverse, []},
+          {ExSlop.Check.Refactor.SortThenAt, []},
+          {ExSlop.Check.Refactor.SortForTopK, []},
+          {ExSlop.Check.Refactor.ListFold, []},
+          {ExSlop.Check.Refactor.ListLast, []},
+          {ExSlop.Check.Refactor.LengthInGuard, []},
+          {ExSlop.Check.Refactor.ExplicitSumReduce, []},
 
           #
           ## Warnings
@@ -151,6 +207,7 @@
           {Credo.Check.Warning.RaiseInsideRescue, []},
           {Credo.Check.Warning.SpecWithStruct, []},
           {Credo.Check.Warning.UnsafeExec, []},
+          {Credo.Check.Warning.UnsafeToAtom, []},
           {Credo.Check.Warning.UnusedEnumOperation, []},
           {Credo.Check.Warning.UnusedFileOperation, []},
           {Credo.Check.Warning.UnusedKeywordOperation, []},
@@ -159,53 +216,45 @@
           {Credo.Check.Warning.UnusedRegexOperation, []},
           {Credo.Check.Warning.UnusedStringOperation, []},
           {Credo.Check.Warning.UnusedTupleOperation, []},
-          {Credo.Check.Warning.WrongTestFileExtension, []}
+          {Credo.Check.Warning.WrongTestFileExtension, []},
+          {ExSlop.Check.Warning.BlanketRescue, []},
+          {ExSlop.Check.Warning.RescueWithoutReraise, []},
+          {ExSlop.Check.Warning.RepoAllThenFilter, []},
+          {ExSlop.Check.Warning.QueryInEnumMap, []},
+          {ExSlop.Check.Warning.GenserverAsKvStore, []},
+          {ExSlop.Check.Warning.PathExpandPriv, []},
+          {ExSlop.Check.Warning.DualKeyAccess, []}
         ],
         disabled: [
           #
-          # Checks scheduled for next check update (opt-in for now)
-          {Credo.Check.Refactor.UtcNowTruncate, []},
-
-          #
           # Controversial and experimental checks (opt-in, just move the check to `:enabled`
-          #   and be sure to use `mix credo --strict` to see low priority checks)
+          # and be sure to use `mix credo --strict` to see low priority checks)
           #
-          {Credo.Check.Consistency.MultiAliasImportRequireUse, []},
           {Credo.Check.Consistency.UnusedVariableNames, []},
           {Credo.Check.Design.DuplicatedCode, []},
-          {Credo.Check.Design.SkipTestWithoutComment, []},
           {Credo.Check.Readability.AliasAs, []},
-          {Credo.Check.Readability.BlockPipe, []},
           {Credo.Check.Readability.ImplTrue, []},
-          {Credo.Check.Readability.MultiAlias, []},
           {Credo.Check.Readability.NestedFunctionCalls, []},
-          {Credo.Check.Readability.OneArityFunctionInPipe, []},
           {Credo.Check.Readability.OnePipePerLine, []},
           {Credo.Check.Readability.SeparateAliasRequire, []},
           {Credo.Check.Readability.SingleFunctionToBlockPipe, []},
-          {Credo.Check.Readability.SinglePipe, []},
           {Credo.Check.Readability.Specs, []},
-          {Credo.Check.Readability.StrictModuleLayout, []},
           {Credo.Check.Readability.WithCustomTaggedTuple, []},
           {Credo.Check.Refactor.ABCSize, []},
-          {Credo.Check.Refactor.AppendSingleItem, []},
-          {Credo.Check.Refactor.DoubleBooleanNegation, []},
           {Credo.Check.Refactor.FilterReject, []},
           {Credo.Check.Refactor.IoPuts, []},
-          {Credo.Check.Refactor.MapMap, []},
           {Credo.Check.Refactor.ModuleDependencies, []},
           {Credo.Check.Refactor.NegatedIsNil, []},
           {Credo.Check.Refactor.PassAsyncInTestCases, []},
-          {Credo.Check.Refactor.PipeChainStart, []},
           {Credo.Check.Refactor.RejectFilter, []},
           {Credo.Check.Refactor.VariableRebinding, []},
           {Credo.Check.Warning.LazyLogging, []},
           {Credo.Check.Warning.LeakyEnvironment, []},
           {Credo.Check.Warning.MapGetUnsafePass, []},
           {Credo.Check.Warning.MixEnv, []},
-          {Credo.Check.Warning.UnsafeToAtom, []}
 
-          # {Credo.Check.Refactor.MapInto, []},
+          # Other
+          {ExSlop.Check.Readability.DocFalseOnPublicFunction, []}
 
           #
           # Custom checks can be created using `mix credo.gen.check`.
--- mix.exs
+++ mix.exs
@@ -1,18 +1,3 @@
-# Copyright (C) 2025 The dspace-ex Project Contributors
-# Copyright (C) 2025 Moritz F. Fürst
-#
-# dspace-ex is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of
-# the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
 defmodule DSpace.MixProject do
   use Mix.Project
 
@@ -25,6 +10,7 @@ defmodule DSpace.MixProject do
       app: :dspace,
       version: @version,
       elixir: "~> 1.18",
+      elixirc_options: elixirc_options(),
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -33,15 +19,25 @@ defmodule DSpace.MixProject do
       docs: docs(),
       aliases: aliases(),
       dialyzer: dialyzer(),
-      preferred_cli_env: preferred_cli_env(),
       source_url: @source_url
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  def cli do
+    [
+      preferred_envs: [precommit: :test, check: :test, "test.ci": :test, dialyzer: :test]
+    ]
+  end
+
+  defp elixirc_options do
+    [
+      no_warn_undefined: [Req]
     ]
   end
 
@@ -49,17 +45,17 @@ defmodule DSpace.MixProject do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:nimble_options, "~> 1.1"},
-      {:req, "~> 0.5", optional: true},
+      {:req, "~> 0.5 or ~> 1.0", optional: true},
 
-      # Dev and Test dependencies
+      # Development and Testing
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_slop, "~> 0.4", only: [:dev, :test], runtime: false},
       {:styler, "~> 1.4", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:muex, "~> 0.6.0", only: [:dev, :test], runtime: false},
       {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false},
       {:bypass, "~> 2.1", only: [:dev, :test]},
       {:stream_data, "~> 1.2", only: [:dev, :test]}
@@ -68,29 +64,26 @@ defmodule DSpace.MixProject do
 
   defp aliases do
     [
+      precommit: [
+        "compile --warnings-as-errors",
+        "deps.unlock --unused",
+        "format",
+        "credo --strict",
+        "dialyzer --quiet-with-result",
+        "test"
+      ],
       check: [
         "format --check-formatted",
         "credo --strict",
-        "dialyzer --quiet-with-result"
-      ],
-      "check.ci": [
-        "format --check-formatted",
+        "dialyzer --quiet-with-result",
         "deps.unlock --check-unused",
-        "hex.audit",
         "deps.audit",
-        "credo --strict",
-        "dialyzer --quiet-with-result"
+        "hex.audit"
       ],
       "test.ci": [
-        "test --raise"
+        "test --raise",
+        "muex --optimize --optimize-level conservative --fail-at 80"
       ]
-    ]
-  end
-
-  defp preferred_cli_env do
-    [
-      "check.ci": :test,
-      "test.ci": :test
     ]
   end
 
@@ -98,7 +91,8 @@ defmodule DSpace.MixProject do
     [
       ignore_warnings: ".dialyzer_ignore.exs",
       plt_core_path: "_build/#{Mix.env()}/plt",
-      plt_file: {:no_warn, "_build/#{Mix.env()}/plt/dialyzer.plt"}
+      plt_file: {:no_warn, "_build/#{Mix.env()}/plt/dialyzer.plt"},
+      plt_add_apps: [:ex_unit]
     ]
   end
 
@@ -113,8 +107,10 @@ defmodule DSpace.MixProject do
 
   defp package do
     [
-      licenses: ["AGPL-3.0-or-later"],
+      name: :dspace_ex,
+      licenses: ["AGPL-3.0-only"],
       maintainers: ["Moritz F. Fürst"],
+      source_url: @source_url,
       links: %{"GitHub" => @source_url}
     ]
   end
